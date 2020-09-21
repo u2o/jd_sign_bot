70eb9e561a5e9c0cf2a3da0ea20c16b7f76cf151
u2o <45503979+u2o@users.noreply.github.com>
2020-09-22 01:22:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const KEY = process.env.JD_COOKIE
 const serverJ = process.env.PUSH_KEY
 const KEY_2 = process.env.JD_COOKIE_2
 const KEY_3 = process.env.JD_COOKIE_3
+const num=1;
 async function downFile () {
     // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
     const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
@@ -46,10 +47,17 @@ async function sendNotify (text,desp) {
 }
 
 async function start() {
-  let cc="0294";
+  let cc="";
   if (!KEY) {
     console.log('请填写 key 后在继续')
     return
+  }
+  if(num==1){
+      cc="94";
+  }else if(num==2){
+      cc="06";
+  }else{
+      cc="90";
   }
   // 下载最新代码
   await downFile();
@@ -70,21 +78,20 @@ async function start() {
     await sendNotify(cc+"京东签到-" + new Date().toLocaleDateString(), content);
     console.log('发送结果完毕')
   }
-  if (KEY_2) {
+  if (KEY_2&&num<2) {
+    KEY=KEY_2;
     console.log('第2个账号开始签到')
-    cc="3706";
-    this.KEY=KEY_2;
     await start();
-    
+    num++;
   }else{
+      num++
       console.log('KEY_2不存在')
   }
-  if (KEY_3) {
+  if (KEY_3&&num<3) {
+    KEY=KEY_3;
     console.log('第3个账号开始签到')
-     cc="3490";
-    this.KEY=KEY_3;
     await start();
-    
+    num++;
   }else{
       console.log('KEY_3不存在')
   }
